1203ea421b8835a3e54df29a8c64cea3fbb40d7e
1he03 <hlever.dev@gmail.com>
2024-09-14 11:34:52 +0300

    .

--- src/buffer.rs
+++ src/buffer.rs
@@ -5,7 +5,7 @@ use raqote::DrawTarget;
 
 pub fn image_to_webp_buffer(dt: &DrawTarget, quality: f32) -> Result<Vec<u8>,  libwebp_sys::WebPEncodingError> {
     let mut img: ImageBuffer<Rgba<u8>, Vec<_>> = image::ImageBuffer::new(dt.width() as u32, dt.height() as u32);
-    raqote_to_image(&dt, &mut img);
+    raqote_to_image_for_webp(&dt, &mut img);
     unsafe {
         let mut out_buf = std::ptr::null_mut();
         let stride = (img.width() * 4) as i32;
@@ -37,6 +37,28 @@ pub fn raqote_to_image(dt: &DrawTarget, img: &mut RgbaImage) {
         let mut r = (pixel >> 16) & 0xffu32;
         let mut g = (pixel >> 8) & 0xffu32;
         let mut b = (pixel >> 0) & 0xffu32;
+
+        if a > 0u32 {
+            r = r * 255u32 / a;
+            g = g * 255u32 / a;
+            b = b * 255u32 / a;
+        }
+
+        let x = i % img.width();
+        let y = i / img.width();
+
+        img.put_pixel(x, y, Rgba([r as u8, g as u8, b as u8, a as u8]));
+        i += 1;
+    }
+}
+
+pub fn raqote_to_image_for_webp(dt: &DrawTarget, img: &mut RgbaImage) {
+    let mut i = 0;
+    for pixel in dt.get_data() {
+        let a = (pixel >> 24) & 0xffu32;
+        let mut r = (pixel >> 0) & 0xffu32;
+        let mut g = (pixel >> 8) & 0xffu32;
+        let mut b = (pixel >> 16) & 0xffu32;
 
         if a > 0u32 {
             r = r * 255u32 / a;
